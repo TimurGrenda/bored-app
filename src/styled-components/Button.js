0c77a11ca64c78e41df49fade1ba1e49527f3276
Timur <timurgrenda@gmail.com>
2018-12-14 17:40:52 +0300

    add disabled prop to SC.Button

--- src/styled-components/Button.js
+++ src/styled-components/Button.js
@@ -1,21 +1,33 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { lighten } from 'polished';
 
-const getBackgroundColor = ({ theme, secondary }) => {
+const getBackgroundColor = ({ theme, secondary, disabled }) => {
   if (secondary) {
-    return theme.secondaryColor;
+    return 'transparent';
+  }
+  if (disabled) {
+    return lighten(0.2, theme.primaryColor);
   }
   return theme.primaryColor;
 };
 
-const getTextColor = ({ theme, secondary }) => {
+const getTextColor = ({ theme, secondary, disabled }) => {
   if (secondary) {
+    if (disabled) {
+      return lighten(0.2, theme.primaryColor);
+    }
     return theme.primaryColor;
   }
   return theme.secondaryColor;
 };
 
-const getBorderColor = ({ theme }) => theme.primaryColor;
+const getBorderColor = ({ theme, disabled }) => {
+  if (disabled) {
+    return lighten(0.2, theme.primaryColor);
+  }
+  return theme.primaryColor;
+};
 
 const Button = styled.button`
   padding: 5px 10px;
@@ -23,7 +35,7 @@ const Button = styled.button`
   background-color: ${getBackgroundColor};
   border: 2px solid ${getBorderColor};
   font-size: 1.2em;
-  cursor: pointer;
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
   text-align: center;
   outline: none;
   min-width: 100px;
@@ -33,6 +45,7 @@ const Button = styled.button`
 
 Button.propTypes = {
   secondary: PropTypes.bool,
+  disabled: PropTypes.bool,
   type: PropTypes.oneOf(['button', 'submit', 'reset']),
 };
 export { Button };
