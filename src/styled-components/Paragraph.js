e175204af2e1d32699ab306be96dd5ce30380c91
Timur <timurgrenda@gmail.com>
2018-12-16 18:39:40 +0300

    add centered prop to SC.Paragraph

--- src/styled-components/Paragraph.js
+++ src/styled-components/Paragraph.js
@@ -1,8 +1,18 @@
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Paragraph = styled.p`
   margin-top: 10px;
   margin-bottom: 0;
+  text-align: ${({ centered }) => (centered ? 'center' : 'inherit')};
 `;
+
+Paragraph.propTypes = {
+  centered: PropTypes.bool,
+};
+
+Paragraph.defaultProps = {
+  centered: false,
+};
 
 export { Paragraph };
