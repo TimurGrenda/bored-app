acafa07a3766a2d5a248542787d66d6dc15c6216
Timur <timurgrenda@gmail.com>
2019-02-13 18:11:17 +0300

    add noTopMargin prop to SC.Paragraph

--- src/styled-components/Paragraph.js
+++ src/styled-components/Paragraph.js
@@ -2,17 +2,19 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const Paragraph = styled.p`
-  margin-top: 10px;
+  margin-top: ${({ noTopMargin }) => (noTopMargin ? '0px' : '10px')};
   margin-bottom: 0;
   text-align: ${({ centered }) => (centered ? 'center' : 'inherit')};
 `;
 
 Paragraph.propTypes = {
   centered: PropTypes.bool,
+  noTopMargin: PropTypes.bool,
 };
 
 Paragraph.defaultProps = {
   centered: false,
+  noTopMargin: false,
 };
 
 export { Paragraph };
