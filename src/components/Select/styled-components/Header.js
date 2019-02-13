a1234797b03ca3e9ffe1fa1af2d9d664b1f7b0f0
Timur <timurgrenda@gmail.com>
2019-02-13 17:55:32 +0300

    remove shadow from Select

--- src/components/Select/styled-components/Header.js
+++ src/components/Select/styled-components/Header.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Header = styled.div`
   position: relative;
   background-color: white;
-  box-shadow: rgba(0, 0, 0, 0.1) 0 0 0 1px, rgba(0, 0, 0, 0.1) 0 4px 11px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0 0 0 1px;
   border-radius: 4px;
 
   user-select: none;
